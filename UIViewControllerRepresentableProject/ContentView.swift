e2f5411fd990ebc8292c8c10cb0fde3d219c2808
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2024-02-08 12:19:43 -0700

    Initial Commit

---
+++ UIViewControllerRepresentableProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UIViewControllerRepresentableProject
+//
+//  Created by Kevin Martinez on 2/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UIViewControllerRepresentableProject/UIViewControllerRepresentableProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewControllerRepresentableProjectApp.swift
+//  UIViewControllerRepresentableProject
+//
+//  Created by Kevin Martinez on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct UIViewControllerRepresentableProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
